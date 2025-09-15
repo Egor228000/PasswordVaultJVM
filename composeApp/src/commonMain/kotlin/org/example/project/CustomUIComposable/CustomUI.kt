e15399088afcb1a507cd3e4b234ebe9a410891bd
Egor <egorkrasnov123456789@gmail.com>
2025-09-15 18:08:42 +0700

    add custom ui

--- composeApp/src/commonMain/kotlin/org/example/project/CustomUIComposable/CustomUI.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/CustomUIComposable/CustomUI.kt
@@ -1,13 +1,24 @@
 package org.example.project.CustomUIComposable
 
 import androidx.annotation.Size
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
@@ -48,8 +59,69 @@ fun CustomTextDescription(
 }
 
 @Composable
-fun CustomTextField(
-
+fun CustomOutlinedTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String,
+    modifier: Modifier = Modifier,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    textTitle: String,
+    singleLine: Boolean = true
 ) {
+    Column {
+
+        CustomTextDescription(
+            text = textTitle,
+            fontSize = 15.sp
+        )
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            shape = RoundedCornerShape(15),
+            placeholder = {
+                CustomTextDescription(
+                    placeholder,
+                    fontWeight = FontWeight.W400,
+                    fontSize = 16.sp
+                )
+            },
+            singleLine = singleLine,
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = MaterialTheme.colorScheme.primary,
+                unfocusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                focusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                focusedIndicatorColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                unfocusedIndicatorColor = MaterialTheme.colorScheme.primaryContainer
+            ),
+            textStyle = TextStyle(
+                fontSize = 17.sp
+            ),
+            trailingIcon = trailingIcon,
+            leadingIcon = leadingIcon,
+            modifier = modifier
+        )
+    }
+}
+
+
+@Composable
+fun CustomButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    shape: Shape = RoundedCornerShape(15.dp),
+    color : ButtonColors = ButtonDefaults.buttonColors()
+) {
+    Button(
+        onClick = {onClick()},
+        modifier = modifier,
+        shape = shape,
+        colors = color
+    ) {
+        CustomTextTitle(
+            text = text
+        )
+    }
 
 }

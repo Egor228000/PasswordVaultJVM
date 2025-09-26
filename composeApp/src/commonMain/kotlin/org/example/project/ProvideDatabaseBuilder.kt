7e788e79814933adbd511818d02a40c765364827
Egor <egorkrasnov123456789@gmail.com>
2025-09-27 00:33:37 +0700

    delete file

--- composeApp/src/commonMain/kotlin/org/example/project/ProvideDatabaseBuilder.kt
+++
@@ -1,6 +0,0 @@
-package org.example.project
-
-import androidx.room.RoomDatabase
-import org.example.project.room.AppDatabase
-
-expect fun provideDatabaseBuilder(): RoomDatabase.Builder<AppDatabase>

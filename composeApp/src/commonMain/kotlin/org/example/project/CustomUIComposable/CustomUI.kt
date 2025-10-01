87925473e99979d47aacd14a1bf78682c8dc09ee
Egor <egorkrasnov123456789@gmail.com>
2025-10-01 19:35:26 +0700

    fix

--- composeApp/src/commonMain/kotlin/org/example/project/CustomUIComposable/CustomUI.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/CustomUIComposable/CustomUI.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -54,7 +55,9 @@ fun CustomTextDescription(
         fontSize = fontSize,
         color = color,
         fontWeight = fontWeight,
-        modifier = modifier
+        modifier = modifier,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 1
     )
 }
 
